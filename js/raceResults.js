10c891b382f69c433df20578263ff4bfce8691e2
NikoP93 <nipe0001@stud.kea.dk>
2025-01-14 22:12:03 +0100

    Result tilføjet

--- js/raceResults.js
+++ js/raceResults.js
@@ -2,6 +2,10 @@ import{getIpAdress,restDelete,fetchAnyUrl,postObjectAsJson} from "./module.js";
 
 const popup = document.querySelector("dialog")
 
+
+//Store det fulde race i localStorage. Brug fetch raceID til url og
+// fetch boattype og brug det til at adde
+
 async function fetchRaceResults(){
     const raceID = window.localStorage.getItem("RaceID");
     const url = getIpAdress() + "/resultsbyraceid/" + raceID
@@ -45,16 +49,7 @@ function putDataInTable(data, index) {
 function editResult(data){
     popup.showModal()
 
-
-
-    const idField = document.getElementById("resultID")
-    idField.value = data.resultID;
-
-    const boatIDField = document.getElementById("boatID")
-    boatIDField.value = data.sailboat.boatID
-
-    const raceIDField = document.getElementById("raceID")
-    raceIDField.value = data.race.raceID
+    addResult()
 
     const positionField = document.getElementById("position")
     positionField.value = data.position
@@ -69,17 +64,26 @@ function editResult(data){
 
     const submitResult = document.getElementById("updateRaceForm")
 
-    submitResult.addEventListener("submit",updateResult)
+
+    submitResult.addEventListener("submit", () => {
+        updateResult2(data)
+    })
 }
 
-async function updateResult(){
-    const form = document.getElementById("updateRaceForm")
-    const formData = new FormData(form)
-    const newObject = Object.fromEntries(formData.entries())
-    console.log("Dette er det nye object: " + newObject)
-    const url = getIpAdress() + "/result/" + newObject.resultID;
+async function updateResult2(data){
+    const positionField = document.getElementById("position")
+    data.position = positionField.value
+
+    const pointsField = document.getElementById("points")
+    data.points = pointsField.value
+
+    const statusField = document.getElementById("status")
+    data.status = statusField.value
+
+    console.log("Dette er det nye object: " + data)
+    const url = getIpAdress() + "/result/" + data.resultID;
     console.log("dette er url: " + url)
-    const response = await postObjectAsJson(url, newObject,"PUT")
+    const response = await postObjectAsJson(url, data,"PUT")
     if(!response.ok){
         alert("Det gik ikke godt med at update")
     } else{
@@ -87,6 +91,64 @@ async function updateResult(){
         popup.close()
         fetchRaceResults()
     }
+}
+
+
+
+async function updateResult(){
+    const form = document.getElementById("updateRaceForm")
+    const formData = new FormData(form)
+    const newObject = Object.fromEntries(formData.entries())
+
+    const updateRequest = {
+        "resultID": newObject.resultID,
+        "position": newObject.position,
+        "points": newObject.points,
+        "status": newObject.status,
+        "sailboat": {
+            boatID: newObject.boatID,
+        },
+        "race":{
+            "raceID":newObject.raceID,
+        }
+    }
+    console.log("Dette er det nye object: " + newObject)
+    const url = getIpAdress() + "/result/" + newObject.resultID;
+    console.log("dette er url: " + url)
+    const response = await postObjectAsJson(url, updateRequest,"PUT")
+    if(!response.ok){
+        alert("Det gik ikke godt med at update")
+    } else{
+        alert("Data er opdateret")
+        popup.close()
+        fetchRaceResults()
+    }
+}
+
+function addResult() {
+
+    const boats = [
+        {
+            "boatID": 1,
+            "boatType": "LONGERTHAN40",
+            "boatName": "F24"
+        },
+        {
+            "boatID": 4,
+            "boatType": "LONGERTHAN40",
+            "boatName": "Hans peter"
+        }
+    ]
+
+    const boatSelect = document.getElementById("boats")
+
+    boats.forEach(function(boat, index) {
+        const op = document.createElement("option")
+        op.setAttribute("value", boat.boatID)
+        op.innerHTML = boat.boatName
+        boatSelect.appendChild(op)
+    });
+
 }
 
 async function deleteResult(data){
